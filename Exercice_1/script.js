20c97928c400b1f725ff5074e0eeacfc0fce4b90
Nadir-Bsd <bensadi.nadir42400@gmail.com>
2024-11-18 11:05:06 +0100

    END: Exercice_1

---
+++ Exercice_1/script.js
@@ -0,0 +1,10 @@
+const img = document.getElementById("image1");
+
+img.addEventListener("mouseover",function () {
+    img.src="./images/image1_2.jpg";
+});
+
+img.addEventListener("mouseout", function () {
+   img.src="./images/image1.jpg";
+});
+
